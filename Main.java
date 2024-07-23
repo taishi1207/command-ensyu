ac62a8376d1fb7bb49d1f57274c4bc5f425255ff
taishi <t.komatsu.sys23@morijyobi.ac.jp>
2024-07-23 11:46:18 +0900

    Main.javaの仕様変更

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class BMI {
 
         System.out.println("BMIを計算して肥満度を表示します。");
 
-        System.out.println("身長を『m』で入力してください(150cmなら1.5と入力してください)");
+        System.out.println("1人目の身長を『m』で入力してください(150cmなら1.5と入力してください)");
         double height = sc.nextDouble();
         System.out.println("体重を『Kg』で入力してください(42.5kgなら42.5と入力してください)");
         double weight = sc.nextDouble();
@@ -31,6 +31,32 @@ public class BMI {
 
         System.out.println("------------------------------------");
         System.out.println("あなたのBMIは" + bmi + "です。");
+        System.out.println("診断結果:" + message);
+        System.out.println("------------------------------------");
+
+        System.out.println("身長を『m』で入力してください(150cmなら1.5と入力してください)");
+        double height2 = sc.nextDouble();
+        System.out.println("体重を『Kg』で入力してください(42.5kgなら42.5と入力してください)");
+        double weight2 = sc.nextDouble();
+
+        double bmi2 = weight2 / (height2 * height);
+
+        if (bmi2 < 18.5) {
+            message = "やせ型";
+        } else if(bmi2 >= 18.5 && bmi2 < 25) {
+            message = "標準体系";
+        } else if( bmi2 >= 25 && bmi2 < 30){
+            message = "肥満1度";
+        } else if(bmi2 >= 30 && bmi2 < 35 ){
+            message = "肥満2度";
+        } else if (bmi2 >= 35 && bmi2 < 40 ){
+            message = "肥満3度";
+        } else if (bmi2 >= 40 ) {
+            message = "肥満4度";
+        }
+
+        System.out.println("------------------------------------");
+        System.out.println("あなたのBMIは" + bmi2 + "です。");
         System.out.println("診断結果:" + message);
         System.out.println("------------------------------------");
     }
