8bb42855c1c7776f39ce584428adf827a7c66eef
taishi <t.komatsu.sys23@morijyobi.ac.jp>
2024-07-23 11:24:57 +0900

    Main.javaとgitignoreの追加

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String message = ""; // 肥満度を表示するメッセージ
+
+        System.out.println("BMIを計算して肥満度を表示します。");
+
+        System.out.println("身長を『m』で入力してください(150cmなら1.5と入力してください)");
+        double height = sc.nextDouble();
+        System.out.println("体重を『Kg』で入力してください(42.5kgなら42.5と入力してください)");
+        double weight = sc.nextDouble();
+
+        double bmi = weight / (height * height);
+
+        if (bmi < 18.5) {
+            message = "やせ型";
+        } else if(bmi >= 18.5 && bmi < 25) {
+            message = "標準体系";
+        } else if( bmi >= 25 && bmi < 30){
+            message = "肥満1度";
+        } else if(bmi >= 30 && bmi < 35 ){
+            message = "肥満2度";
+        } else if (bmi >= 35 && bmi < 40 ){
+            message = "肥満3度";
+        } else if (bmi >= 40 ) {
+            message = "肥満4度";
+        }
+
+        System.out.println("------------------------------------");
+        System.out.println("あなたのBMIは" + bmi + "です。");
+        System.out.println("診断結果:" + message);
+        System.out.println("------------------------------------");
+    }
+}
+
